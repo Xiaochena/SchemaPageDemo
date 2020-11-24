680410bc6b64601671d34d0d48408066c3f0c216
陈林港 <Clare>
2020-11-24 14:27:51 +0800

    [style] tableSchema

--- src/pages/SchemaPageDemo/tableSchema.ts
+++ src/pages/SchemaPageDemo/tableSchema.ts
@@ -3,54 +3,6 @@
  */
 import { TableSchemaType } from 'schema-page/dist/types';
 
-const schemaTableProps: TableSchemaType = {
-  formType: 'schemaForm',
-  pagination: { pageSize: 10 },
-  formProps: '{{formProps}}',
-  modalProps: {
-    width: 500,
-    title: '运营人员',
-  },
-  operationProps: {
-    operations: ['{{viewWeChatFriends}}'],
-    width: 70,
-  },
-  scroll: { x: 1000 },
-  rowKey: 'FriendID',
-  columns: [
-    {
-      title: '微信昵称',
-      dataIndex: 'NickName',
-      key: 'NickName',
-      width: 200,
-    },
-    {
-      title: '好友备注',
-      dataIndex: 'Remark',
-      key: 'Remark',
-      width: 200,
-    },
-    {
-      title: '微信号',
-      dataIndex: 'WxAccount',
-      key: 'WxAccount',
-      width: 200,
-    },
-    {
-      title: '标签',
-      dataIndex: 'tags',
-      key: 'tags',
-      width: 200,
-    },
-    {
-      title: '所属机器人',
-      dataIndex: 'weixins',
-      key: 'weixins',
-      width: 200,
-    },
-  ],
-};
-
 export default {
   type: 'object',
   properties: {
@@ -58,7 +10,53 @@ export default {
       name: 'NO_NAME_FIELD_$0',
       type: 'object',
       'x-component': 'schemaTable',
-      'x-component-props': schemaTableProps,
+      'x-component-props': {
+        formType: 'schemaForm',
+        pagination: { pageSize: 10 },
+        formProps: '{{formProps}}',
+        modalProps: {
+          width: 500,
+          title: '运营人员',
+        },
+        operationProps: {
+          operations: ['{{viewWeChatFriends}}'],
+          width: 70,
+        },
+        scroll: { x: 1000 },
+        rowKey: 'FriendID',
+        columns: [
+          {
+            title: '微信昵称',
+            dataIndex: 'NickName',
+            key: 'NickName',
+            width: 200,
+          },
+          {
+            title: '好友备注',
+            dataIndex: 'Remark',
+            key: 'Remark',
+            width: 200,
+          },
+          {
+            title: '微信号',
+            dataIndex: 'WxAccount',
+            key: 'WxAccount',
+            width: 200,
+          },
+          {
+            title: '标签',
+            dataIndex: 'tags',
+            key: 'tags',
+            width: 200,
+          },
+          {
+            title: '所属机器人',
+            dataIndex: 'weixins',
+            key: 'weixins',
+            width: 200,
+          },
+        ],
+      } as TableSchemaType,
     },
   },
 };
